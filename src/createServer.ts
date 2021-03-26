7baa6b020aceb5f9453c202c5a6c4bd45da94b3a
Mockingbird <hide0mockingbird@gmail.com>
2021-03-26 10:32:20 +0700

    backend setup

---
+++ src/createServer.ts
@@ -0,0 +1,52 @@
+import { UserModel } from "./entities/User";
+import { ApolloServer, gql } from "apollo-server-express";
+
+const typeDefs = gql`
+  type User {
+    id: String!
+    username: String!
+    email: String!
+    password: String!
+  }
+
+  type Query {
+    users: [User]!
+  }
+
+  type Mutation {
+    createUser(username: String!, email: String!, password: String!): User
+  }
+`;
+
+interface InputAtgs {
+  username: string;
+  email: string;
+  password: string;
+}
+
+const resolvers = {
+  Query: {
+    users: () => UserModel.find(),
+  },
+  Mutation: {
+    createUser: async (_: any, args: InputAtgs) => {
+      try {
+        const { username, email, password } = args;
+
+        const newUser = await UserModel.create({
+          username,
+          email,
+          password,
+        });
+
+        return newUser;
+      } catch (error) {
+        throw error;
+      }
+    },
+  },
+};
+
+export default () => {
+  return new ApolloServer({ typeDefs, resolvers });
+};
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,37 @@
 import express from "express";
+import monggoose from "mongoose";
+import { config } from "dotenv";
+import createServer from "./createServer";
+config();
 
-const startServer = () => {
-  const app = express();
+const { PORT, DB_USER, DB_PASSWORD, DB_ENDPOINT, DB_NAME } = process.env;
 
-  app.listen({ port: 5000 }, () => {
-    console.log(`Server is ready at http://localhost:5000`);
-  });
+const startServer = async () => {
+  try {
+    await monggoose.connect(
+      `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_ENDPOINT}/${DB_NAME}?retryWrites=true&w=majority`,
+      {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      }
+    );
+
+    const app = express();
+
+    const server = createServer();
+
+    server.applyMiddleware({ app });
+
+    app.listen({ port: PORT }, () => {
+      console.log(
+        `Server is ready at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 startServer();
