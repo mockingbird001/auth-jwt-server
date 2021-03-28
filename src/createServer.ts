a4437ee809cfed9bd2249f5cd001731a5f1c7deb
Mockingbird <hide0mockingbird@gmail.com>
2021-03-28 21:01:16 +0700

    token expiration

--- src/createServer.ts
+++ src/createServer.ts
@@ -1,8 +1,9 @@
 import { AppContext } from "./types/index";
-import { verifyToken } from "./utils/tokenHandler";
+import { createToken, sendToken, verifyToken } from "./utils/tokenHandler";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { AuthResolvers } from "./resolvers/AuthResolvers";
+import { UserModel } from "./entities/User";
 
 export default async () => {
   const schema = await buildSchema({
@@ -13,7 +14,7 @@ export default async () => {
 
   return new ApolloServer({
     schema,
-    context: ({ req, res }: AppContext) => {
+    context: async ({ req, res }: AppContext) => {
       const token = req.cookies[process.env.COOKIE_NAME!];
 
       if (token) {
@@ -28,6 +29,28 @@ export default async () => {
           if (decodedToken) {
             req.userId = decodedToken.userId;
             req.tokenVersion = decodedToken.tokenVersion;
+
+            if (Date.now() / 1000 - decodedToken.iat > 6 * 60 * 60) {
+              const user = await UserModel.findById(req.userId);
+
+              if (user) {
+                if (user.tokenVersion === req.tokenVersion) {
+                  user.tokenVersion = user.tokenVersion + 1;
+                  const updatedUser = await user.save();
+
+                  if (updatedUser) {
+                    const token = createToken(
+                      updatedUser.id,
+                      updatedUser.tokenVersion
+                    );
+
+                    req.tokenVersion = updatedUser.tokenVersion;
+
+                    sendToken(res, token);
+                  }
+                }
+              }
+            }
           }
         } catch (error) {
           req.userId = undefined;
